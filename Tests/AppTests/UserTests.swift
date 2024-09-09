8aa5cd5a1a25aa8c6733f6c96d6b69baec03a0df
Reginald Maltais <reginald.maltais@gmail.com>
2024-09-08 20:23:32 -0400

    Add additional tests for user routes

--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -1,11 +1,16 @@
 @testable import App
 import Fakery
 import Fluent
+import Vapor
 import XCTVapor
 
 final class UserTests: XCTestCase {
     var app: Application!
     let faker = Faker()
+    
+    // some sample users
+    let firstUserEmail = Environment.get("FIRST_SUPERUSER") ?? "admin@example.com"
+    let firstUserPassword = Environment.get("FIRST_SUPERUSER_PASSWORD") ?? "changethis"
     
     override func setUp() async throws {
         self.app = try await Application.make(.testing)
@@ -14,6 +19,10 @@ final class UserTests: XCTestCase {
     }
     
     override func tearDown() async throws {
+        // Clean up any tokens
+        let registeredTokens = try await UserToken.query(on: self.app.db).all()
+        try await registeredTokens.delete(on: self.app.db)
+        
         try await app.autoRevert()
         try await self.app.asyncShutdown()
         self.app = nil
@@ -66,7 +75,7 @@ final class UserTests: XCTestCase {
         })
     }
     
-    func testCreateUserAsSuperUser() async throws {
+    func testCreateUserNewEmail() async throws {
         // Register a user to act as
         let email = faker.internet.email()
         let password = faker.internet.password()
@@ -91,6 +100,73 @@ final class UserTests: XCTestCase {
             XCTAssertEqual(createdUser.isActive, newUser.isActive)
             XCTAssertEqual(createdUser.isSuperuser, newUser.isSuperuser)
             XCTAssertNotNil(createdUser.id)
+        })
+        
+        // Look the user up in the DB
+        let savedUser = try await User.query(on: self.app.db).filter(\.$email == newUser.email).first()
+        
+        if let user = savedUser {
+            XCTAssertEqual(user.email, newUser.email)
+        } else {
+            XCTFail("Unable to retrieve created user \(newUser.email)")
+        }
+    }
+    
+    func testGetUsersSuperuserMe() async throws {
+        let firstUser = try await User.query(on: self.app.db).filter(\.$email == self.firstUserEmail).first()
+        let firstUserToken = try XCTUnwrap(firstUser?.generateToken())
+        try await firstUserToken.save(on: self.app.db)
+        
+        try await self.app.test(.GET, "users/me", beforeRequest: { req in
+            req.headers.bearerAuthorization = BearerAuthorization(token: firstUserToken.value)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            
+            let currentUser = try res.content.decode(UserDTO.self)
+            XCTAssertTrue(currentUser.isActive)
+            XCTAssertTrue(currentUser.isSuperuser)
+            XCTAssertEqual(currentUser.email, self.firstUserEmail)
+        })
+    }
+    
+    func testGetUsersNormalUserMe() async throws {
+        // Register a user to act as
+        let me = User(fullName: faker.name.name(), email: faker.internet.email(), isActive: true, isSuperuser: false, hashedPassword: try Bcrypt.hash(faker.internet.password()))
+        try await me.save(on: self.app.db)
+        
+        let myToken = try me.generateToken()
+        try await myToken.save(on: self.app.db)
+        
+        try await self.app.test(.GET, "users/me", beforeRequest: { req in
+            req.headers.bearerAuthorization = BearerAuthorization(token: myToken.value)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            
+            let currentUser = try res.content.decode(UserDTO.self)
+            XCTAssertTrue(currentUser.isActive)
+            XCTAssertFalse(currentUser.isSuperuser)
+            XCTAssertEqual(currentUser.email, me.email)
+        })
+    }
+    
+    func testGetExistingUser() async throws {
+        let sampleUser = User(fullName: faker.name.name(), email: faker.internet.email(), isActive: true, isSuperuser: false, hashedPassword: try Bcrypt.hash(faker.internet.password()))
+        try await sampleUser.save(on: self.app.db)
+        
+        let firstUser = try await User.query(on: self.app.db).filter(\.$email == self.firstUserEmail).first()
+        let firstUserToken = try XCTUnwrap(firstUser?.generateToken())
+        try await firstUserToken.save(on: self.app.db)
+        
+        // Look up the ID of the user we created so we can form a request based on it
+        let searchUser = try await User.query(on: self.app.db).filter(\.$email == sampleUser.email).first()
+        let searchUserId = try XCTUnwrap(searchUser?.id)
+        try await self.app.test(.GET, "users/\(searchUserId)", beforeRequest: { req in
+            req.headers.bearerAuthorization = BearerAuthorization(token: firstUserToken.value)
+        }, afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            
+            let chosenUser = try res.content.decode(UserDTO.self)
+            XCTAssertEqual(chosenUser.email, sampleUser.email)
         })
     }
 }
